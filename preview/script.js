f1f42dc224d23a921c085ccad17ebbf607b52c61
Adam Bergman <adam@fransvilhelm.com>
2019-01-17 13:41:01 +0100

    Rework preview using react

--- preview/script.js
+++ preview/script.js
@@ -1,40 +1,66 @@
+import React, { useState, useCallback, useMemo, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+
 const urls = {
-  cdn: '<cdn_url>',
-  apig: '<apig_url>',
+  cdn: 'https://api.fransvilhelm.com/cat',
+  apig:
+    'https://e4i1fvixd2.execute-api.eu-north-1.amazonaws.com/development/cat',
 };
 
-const rootEl = document.getElementById('root');
-const radioUseCdn = document.getElementById('use-cdn');
-const radioUseApig = document.getElementById('use-apig');
-const checkboxUseWebp = document.getElementById('use-webp');
+function App() {
+  const [url, setUrl] = useState(urls.cdn);
+  const [ignoreWebp, setIgnoreWebp] = useState(true);
 
-let use = 'apig';
-let webp = false;
+  const handleUrlChange = useCallback(event => setUrl(event.target.value));
+  const handleWebpChange = useCallback(() => setIgnoreWebp(!ignoreWebp));
+  const finalUrl = useMemo(() => `${url}${ignoreWebp ? '?webp=0' : ''}`, [
+    url,
+    ignoreWebp,
+  ]);
 
-function render() {
-  const imgUrl = `${urls[use]}/cat${webp ? '?webp=1' : ''}`;
+  return (
+    <div>
+      <div>
+        <label htmlFor="use-cdn">
+          <input
+            type="radio"
+            name="url"
+            id="use-cdn"
+            value={urls.cdn}
+            checked={url === urls.cdn}
+            onChange={handleUrlChange}
+          />
+          Use CDN
+        </label>
+        <label htmlFor="use-apig">
+          <input
+            type="radio"
+            name="url"
+            id="use-apig"
+            value={urls.apig}
+            checked={url === urls.apig}
+            onChange={handleUrlChange}
+          />
+          Use API Gateway
+        </label>
 
-  const prevImg = rootEl.querySelector('#image');
-  if (prevImg) rootEl.removeChild(prevImg);
+        <label htmlFor="use-webp">
+          <input
+            type="checkbox"
+            id="use-webp"
+            checked={ignoreWebp}
+            onChange={handleWebpChange}
+          />
+          Ignore WebP support
+        </label>
+      </div>
 
-  const newImg = document.createElement('img');
-  newImg.setAttribute('id', 'image');
-  newImg.setAttribute('src', imgUrl);
-
-  rootEl.appendChild(newImg);
+      <div>
+        <img src={finalUrl} alt="A small cute kitten" />
+      </div>
+    </div>
+  );
 }
 
-checkboxUseWebp.addEventListener('change', event => {
-  webp = event.target.checked;
-  render();
-});
-
-const handler = event => {
-  use = event.target.value;
-  render();
-};
-
-radioUseCdn.addEventListener('change', handler);
-radioUseApig.addEventListener('change', handler);
-
-render();
+const rootEl = document.getElementById('root');
+ReactDOM.render(<App />, rootEl);
