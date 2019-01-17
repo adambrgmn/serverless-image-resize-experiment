0fbb1f06a6fde173482c29d4736af985041c2af6
Adam Bergman <adam@fransvilhelm.com>
2019-01-17 22:00:37 +0100

    Update preview

--- preview/script.js
+++ preview/script.js
@@ -2,24 +2,71 @@ import React, { useState, useCallback, useMemo, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 const urls = {
-  cdn: 'https://api.fransvilhelm.com/cat',
-  apig:
-    'https://e4i1fvixd2.execute-api.eu-north-1.amazonaws.com/development/cat',
+  cdn: 'https://api.fransvilhelm.com/',
+  apig: 'https://e4i1fvixd2.execute-api.eu-north-1.amazonaws.com/development/',
+};
+
+const imgs = {
+  cat: 'cat.jpg',
+  dog: 'dog.jpg',
 };
 
 function App() {
   const [url, setUrl] = useState(urls.cdn);
-  const [ignoreWebp, setIgnoreWebp] = useState(true);
+  const [img, setImg] = useState(imgs.cat);
+  const [query, setQuery] = useState({ w: 400, h: 0, ignore_webp: false });
 
-  const handleUrlChange = useCallback(event => setUrl(event.target.value));
-  const handleWebpChange = useCallback(() => setIgnoreWebp(!ignoreWebp));
-  const finalUrl = useMemo(() => `${url}${ignoreWebp ? '?webp=0' : ''}`, [
-    url,
-    ignoreWebp,
-  ]);
+  const handleUrlChange = useCallback(e => setUrl(e.target.value));
+  const handleImgChange = useCallback(e => setImg(e.target.value));
+
+  const handleWebpChange = useCallback(() =>
+    setQuery({ ...query, ignore_webp: !query.ignore_webp }),
+  );
+  const handleWidthChange = useCallback(e =>
+    setQuery({ ...query, w: Number.parseInt(e.target.value) }),
+  );
+  const handleHeightChange = useCallback(e =>
+    setQuery({ ...query, h: Number.parseInt(e.target.value) }),
+  );
+
+  const finalUrl = useMemo(
+    () =>
+      `${url}${img}?${Object.entries(query)
+        .reduce((q, [key, val]) => {
+          if (!val) return q;
+          return [...q, `${key}=${val}`];
+        }, [])
+        .join('&')}`,
+    [url, img, query],
+  );
 
   return (
     <div>
+      <div>
+        <label htmlFor="use-cat">
+          <input
+            type="radio"
+            name="img"
+            id="use-cat"
+            value={imgs.cat}
+            checked={img === imgs.cat}
+            onChange={handleImgChange}
+          />{' '}
+          /cat.jpg
+        </label>
+        <label htmlFor="use-dog">
+          <input
+            type="radio"
+            name="img"
+            id="use-dog"
+            value={imgs.dog}
+            checked={img === imgs.dog}
+            onChange={handleImgChange}
+          />{' '}
+          /dog.jpg
+        </label>
+      </div>
+
       <div>
         <label htmlFor="use-cdn">
           <input
@@ -43,15 +90,38 @@ function App() {
           />
           Use API Gateway
         </label>
-
-        <label htmlFor="use-webp">
+        <label htmlFor="ignore-webp">
           <input
             type="checkbox"
-            id="use-webp"
-            checked={ignoreWebp}
+            name="ignore-webp"
+            id="ignore-webp"
+            checked={query.ignore_webp}
             onChange={handleWebpChange}
           />
           Ignore WebP support
+        </label>
+      </div>
+
+      <div>
+        <label htmlFor="width">
+          Width:{' '}
+          <input
+            type="number"
+            id="width"
+            value={query.w}
+            onChange={handleWidthChange}
+            step="100"
+          />
+        </label>
+        <label htmlFor="height">
+          Height:{' '}
+          <input
+            type="number"
+            id="height"
+            value={query.h}
+            onChange={handleHeightChange}
+            step="100"
+          />
         </label>
       </div>
 
